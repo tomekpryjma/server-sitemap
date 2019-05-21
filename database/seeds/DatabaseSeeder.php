fac503e56ef29193d8a43bf326522b510008238f
Tomek <tompe97@tlen.pl>
2019-05-21 22:34:34 +0100

    Envoke Users & Servers seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(UsersTableSeeder::class);
+        $this->call(ServersTableSeeder::class);
         $this->call(SitesTableSeeder::class);
     }
 }
