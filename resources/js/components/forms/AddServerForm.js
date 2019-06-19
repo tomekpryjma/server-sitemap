f42c13c0785d92b16ea9bb99dac274a360d7fb7a
Tomek <tompe97@tlen.pl>
2019-06-19 19:36:58 +0100

    Add a AddServerForm.js component

--- resources/js/components/forms/AddServerForm.js
+++ resources/js/components/forms/AddServerForm.js
@@ -7,13 +7,66 @@ export default class extends React.Component {
     }
 
     render() {
-        return(
+        return(    
+        // <Formik
+        //     initialValues={user /** { email, social } */}
+        //     onSubmit={(values, actions) => {
+        //     MyImaginaryRestApiCall(user.id, values).then(
+        //         updatedUser => {
+        //         actions.setSubmitting(false);
+        //         updateUser(updatedUser);
+        //         onClose();
+        //         },
+        //         error => {
+        //         actions.setSubmitting(false);
+        //         actions.setErrors(transformMyRestApiErrorsToAnObject(error));
+        //         actions.setStatus({ msg: 'Set some arbitrary status or data' });
+        //         }
+        //     );
+        //     }}
+        //     render={({ errors, status, touched, isSubmitting }) => (
+        //     <Form>
+        //         <Field type="email" name="email" />
+        //         <ErrorMessage name="email" component="div" />  
+        //         <Field type="text" className="error" name="social.facebook" />
+        //         <ErrorMessage name="social.facebook">
+        //         {errorMessage => <div className="error">{errorMessage}</div>}
+        //         </ErrorMessage>
+        //         <Field type="text" name="social.twitter" />
+        //         <ErrorMessage name="social.twitter" className="error" component="div"/>  
+        //         {status && status.msg && <div>{status.msg}</div>}
+        //         <button type="submit" disabled={isSubmitting}>
+        //         Submit
+        //         </button>
+        //     </Form>
+        //     )}
+        // />
+
+
             <Formik
                 initialValues={{
                     name: '',
                     ip_address: ''
                 }}
-                />
+
+                onSubmit={(name, ip_address) => {
+                    console.log("Name: " + name);
+                    console.log("IP Addr: " + ip_address);
+                }}
+
+                render={({errors, status, touched, isSubmitting}) => (
+                    <Form>
+                        <Field type="email" name="email" />
+                        <ErrorMessage name="email" component="div" />
+                        <Field type="text" name="social.twitter" />
+                        <ErrorMessage name="social.twitter" className="error" component="div"/>  
+                        {status && status.msg && <div>{status.msg}</div>}
+                        <button type="submit" disabled={isSubmitting}>
+                        Submit
+                        </button>
+                    </Form>
+                )}
+            />
         )
     }
 }
