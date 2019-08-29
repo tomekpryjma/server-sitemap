1adddf4d58e185738dfe0b66899d861a2944b29d
tomekevolve <tomek@evolvedigital.co.uk>
2019-08-29 16:34:50 +0100

    Add Client views

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -5,7 +5,42 @@
 @section('content')
 
 <div class="row">
-    
+    <div class="col-12 col-md-8">
+
+        <div class="table-responsive">
+            <table class="table table-hover table-striped">
+
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Website</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    
+                @foreach ($clients as $client)
+                    
+                    <tr>
+                        <th scope="row">{{ $client->id }}</th>
+                        <td>{{ $client->name }}</td>
+                        <td>{{ $client->website }}</td>
+                        <td>
+                            <a href="" title="Edit client information" class="action">
+                                <i data-feather="tool"></i>
+                            </a>
+                        </td>
+                    </tr>
+
+                @endforeach
+
+                </tbody>
+            </table>
+        </div>
+
+    </div>
 </div>
 
 @endsection
--- resources/views/client/show.blade.php
+++ resources/views/client/show.blade.php
@@ -10,50 +10,73 @@
     <div class="row mt-5">
         <div class="col-12 col-md-8">
 
-            <ul class="list-group">
-                <li class="list-group-item">
-                    <strong>
-                        Company name:
-                    </strong>
-                    {{ $client->name }}
-                </li>
+            <div class="card client">
+                <div class="card-body">
 
-                
-                <li class="list-group-item">
-                    <strong>
-                        Contacts:
-                    </strong>
-                    
-                    @foreach (json_decode($client->contacts) as $contact)
+                    <ul class="list-group">
+                        <li class="list-group-item">
+                            <strong>
+                                Company name:
+                            </strong>
+                            {{ $client->name }}
+                        </li>
+        
+                        <li class="list-group-item">
+                            <strong>
+                                Website:
+                            </strong>
+                            <a href="//{{ $client->website }}" target="_blank" rel="noreferrer noopener" class="site-link d-inline-block">
+                                {{ $client->website }}
+                                <span class="d-inline-block link-icon">
+                                    <i data-feather="external-link"></i>
+                                </span>
+                            </a>
+                        </li>
+                    </ul>
+            
+                </div>
+            </div>
+        </div>
+    </div>
 
-                        <ul class="list-group mt-4">
-                            
-                            @foreach ($contact as $key => $value)
+    <div class="row mt-5">
+        <div class="col-12">
+            <section>
+                <h3>Contacts within the client's company</h3>
+
+                <hr>
+
+                <div class="card-deck mt-5 flex-lg-wrap justify-content-md-between">
+
+                @foreach (json_decode($client->contacts) as $contact)
+        
+                    <article class="card contact my-3">
+                        <div class="card-body">
+
+                            <div class="name-tag p-2 px-2 px-lg-4 text-center text-lg-left">
+                                <h4 class="h6 font-weight-bold">{{ $contact->name }}</h4>
+                            </div>
+
+                            <ul class="list-group mt-3">
+
                                 <li class="list-group-item">
-                                    <strong>{{ $key }}</strong>
-                                    {{ $value }}
+                                    <span class="font-weight-bold text-capitalise">Email:</span>
+                                    <a href="mailto:{{ $contact->email }}">{{ $contact->email }}</a>
                                 </li>
-                            @endforeach
 
-                        </ul>
+                                <li class="list-group-item">
+                                    <span class="font-weight-bold text-capitalise">Phone:</span>
+                                    <a href="tel:{{ $contact->phone }}">{{ $contact->phone }}</a>
+                                </li>
 
-                    @endforeach
-
-                </li>
-
-                <li class="list-group-item">
-                    <strong>
-                        Website:
-                    </strong>
-                    <a href="//{{ $client->website }}" target="_blank" rel="noreferrer noopener" class="site-link d-inline-block">
-                        {{ $client->website }}
-                        <span class="d-inline-block link-icon">
-                            <i data-feather="external-link"></i>
-                        </span>
-                    </a>
-                </li>
-            </ul>
-
+                            </ul>
+                        </div>
+                    </article>
+                        
+                @endforeach
+                    
+                </div>
+            </section>
         </div>
     </div>
 @endsection
