4bb95b1b7cb4aa212e6a5127140d1be1113bc9c5
tomekevolve <tomek@evolvedigital.co.uk>
2019-08-30 14:46:26 +0100

    Remove DB id from Client index table

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -7,12 +7,11 @@
 <div class="row">
     <div class="col-12 col-md-8">
 
-        <div class="table-responsive">
-            <table class="table table-hover table-striped">
+        <div class="table-responsive bordered">
+            <table class="table table-hover table-striped mb-0">
 
                 <thead>
                     <tr>
-                        <th>ID</th>
                         <th>Name</th>
                         <th>Website</th>
                         <th>Actions</th>
@@ -24,7 +23,6 @@
                 @foreach ($clients as $client)
                     
                     <tr>
-                        <th scope="row">{{ $client->id }}</th>
                         <td>{{ $client->name }}</td>
                         <td>{{ $client->website }}</td>
                         <td>
