bc5706d511cfdc9ce8d66babd1f1039cbad5fdad
Tomek <tompe97@tlen.pl>
2020-03-22 09:22:29 +0000

    Add "add key contact" vue template

---
+++ resources/js/components/client/AddKeyContact.vue
@@ -0,0 +1,19 @@
+<template>
+    <article class="card contact my-3 add-key-contact">
+        <div class="card-body">
+            <button class="add-contact">
+                <span>
+                    Add a contact&nbsp;<i data-feather="plus-circle"></i>
+                </span>
+            </button>
+        </div>
+    </article>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
