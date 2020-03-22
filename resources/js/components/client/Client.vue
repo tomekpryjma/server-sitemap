412399ff8a31425e89e1237516d7d44205e50c30
Tomek <tompe97@tlen.pl>
2020-03-22 09:22:40 +0000

    Add Client Vue template

---
+++ resources/js/components/client/Client.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <h1 class="page-title h2">
+            <strong>{{ this.clientParsed.name }}</strong>'s details
+        </h1>
+
+
+        <client-details
+            :name="this.clientParsed.name"
+            :website="this.clientParsed.website"
+            :notes="this.clientParsed.notes"/>
+
+        <template v-if="this.clientParsed.contacts">
+            <key-contacts
+                :contacts="this.clientParsed.contacts"/>
+        </template>
+
+        <template v-else>
+            <add-key-contact/>
+        </template>
+
+    </div>
+</template>
+
+<script>
+    import './ClientDetails';
+    import './KeyContacts';
+    import './AddKeyContact';
+
+    export default {
+        props: ['client'],
+
+        data: function() {
+            return {
+                clientParsed: JSON.parse(this.client)
+            }
+        },
+
+        mounted: function() {
+            console.log(this.clientParsed)
+        }
+    }
+</script>
