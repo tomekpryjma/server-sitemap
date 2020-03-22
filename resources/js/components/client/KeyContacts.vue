9b34b8c22759c87d5dcfcfab06483c32c9ac03e9
Tomek <tompe97@tlen.pl>
2020-03-22 09:22:48 +0000

    Add key contacts listing vue template

---
+++ resources/js/components/client/KeyContacts.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="row mt-5">
+        <div class="col-12">
+            <section>
+                <h3>Contacts within the client's company</h3>
+
+                <hr>
+
+                <div class="card-deck mt-5 flex-lg-wrap justify-content-md-between">
+
+                    <template v-for="contact in this.contactsParsed">
+                        <single-contact
+                            :key="contact.name"
+                            :contact="contact"/>
+                    </template>
+
+                    <add-key-contact/>
+                    
+                </div>
+            </section>
+        </div>
+    </div>
+</template>
+
+<script>
+    import './SingleContact';
+    import './AddKeyContact';
+
+    export default {
+        props: ['contacts'],
+
+        data: function() {
+            return {
+                contactsParsed: JSON.parse(this.contacts)
+            }
+        },
+
+        mounted: function() {
+            console.log(this.contactsParsed)
+        }
+    }
+</script>
